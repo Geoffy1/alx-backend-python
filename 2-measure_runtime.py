c85b26c0ff909628a521f7e0d6172f1a5a9dd108
Geoffy1 <geojogu@gmail.com>
2023-05-02 14:25:00 -0700

    my commit

---
+++ 2-measure_runtime.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+'''Task two
+'''
+import asyncio
+import time
+
+
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+def measure_time(n: int, max_delay: int) -> float:
+    """
+    Measure the runtime of wait_n fn
+    """
+    start = time.perf_counter()
+    asyncio.run(wait_n(n, max_delay))
+    end = time.perf_counter()
+    total_time = end - start
+    return total_time / n
