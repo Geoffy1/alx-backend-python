abc80710cd9da1bab29505849d58b6e6409da551
Geoffy1 <106971868+Geoffy1@users.noreply.github.com>
2023-05-26 19:22:41 +0300

    Update test_client.py

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -24,6 +24,15 @@ class TestGithubOrgClient(unittest.TestCase):
         self.assertEqual(gh_org_client.org(), resp)
         mocked_fxn.assert_called_once_with("https://api.github.com/orgs/{}".format(org))
 
+    def test_public_repos_url(self):
+        """Tests the `_public_repos_url` property."""
+        with patch.object(GithubOrgClient, "org", new_callable=PropertyMock) as mock_org:
+            mock_org.return_value = {'repos_url': "https://api.github.com/users/google/repos"}
+            self.assertEqual(
+                GithubOrgClient("google")._public_repos_url,
+                "https://api.github.com/users/google/repos",
+            )
+
 
 if __name__ == '__main__':
     unittest.main()
