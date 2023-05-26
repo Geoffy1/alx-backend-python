e6cdab46b1671a138edd988c18b190b887dd1990
Geoffy1 <106971868+Geoffy1@users.noreply.github.com>
2023-05-26 19:47:25 +0300

    Update test_client.py

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -1,14 +1,10 @@
 import unittest
-from unittest.mock import patch
-from parameterized import parameterized_class
+from unittest.mock import patch, MagicMock
+from parameterized import parameterized
 from fixtures import org_payload, repos_payload, expected_repos, apache2_repos
 from client import GithubOrgClient
 
 
-@parameterized_class([
-    {"org_payload": org_payload, "repos_payload": repos_payload,
-     "expected_repos": expected_repos, "apache2_repos": apache2_repos}
-])
 class TestIntegrationGithubOrgClient(unittest.TestCase):
     """Integration tests for GithubOrgClient."""
 
@@ -26,24 +22,24 @@ class TestIntegrationGithubOrgClient(unittest.TestCase):
     def test_public_repos(self):
         """Test the public_repos method."""
         self.mock_get.side_effect = [
-            MagicMock(json=lambda: self.org_payload),
-            MagicMock(json=lambda: self.repos_payload),
+            MagicMock(json=lambda: org_payload),
+            MagicMock(json=lambda: repos_payload),
         ]
         github_org_client = GithubOrgClient("test")
         repos = github_org_client.public_repos()
-        self.assertEqual(repos, self.expected_repos)
+        self.assertEqual(repos, expected_repos)
         self.mock_get.assert_called_with('https://api.github.com/orgs/test')
         self.mock_get.assert_called_with('https://api.github.com/orgs/test/repos')
 
     def test_public_repos_with_license(self):
         """Test the public_repos method with license filter."""
         self.mock_get.side_effect = [
-            MagicMock(json=lambda: self.org_payload),
-            MagicMock(json=lambda: self.repos_payload),
+            MagicMock(json=lambda: org_payload),
+            MagicMock(json=lambda: repos_payload),
         ]
         github_org_client = GithubOrgClient("test")
-        repos = github_org_client.public_repos("apache-2.0")
-        self.assertEqual(repos, self.apache2_repos)
+        repos = github_org_client.public_repos(license="apache-2.0")
+        self.assertEqual(repos, apache2_repos)
         self.mock_get.assert_called_with('https://api.github.com/orgs/test')
         self.mock_get.assert_called_with('https://api.github.com/orgs/test/repos')
 
