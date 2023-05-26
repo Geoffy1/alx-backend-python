4a4d96f2fa09b59e28976361e71c50ccde30344e
Geoffy1 <106971868+Geoffy1@users.noreply.github.com>
2023-05-26 19:37:26 +0300

    Update test_client.py

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -75,6 +75,15 @@ class TestGithubOrgClient(unittest.TestCase):
         mock_public_repos_url.assert_called_once()
         mock_get_json.assert_called_once()
 
+    @parameterized.expand([
+        ({"license": {"key": "my_license"}}, "my_license", True),
+        ({"license": {"key": "other_license"}}, "my_license", False),
+    ])
+    def test_has_license(self, repo, license_key, expected_result):
+        """Tests the `has_license` method."""
+        github_org_client = GithubOrgClient("google")
+        self.assertEqual(github_org_client.has_license(repo, license_key), expected_result)
+
 
 if __name__ == '__main__':
     unittest.main()
