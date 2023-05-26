b78f1206b918dcab5d5395e173204234711138ce
Geoffy1 <106971868+Geoffy1@users.noreply.github.com>
2023-05-26 19:19:56 +0300

    Update test_client.py

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -1,62 +1,29 @@
 #!/usr/bin/env python3
-"""A module for testing the utils module."""
+"""A module for testing the client module."""
 import unittest
-from typing import Dict, Tuple, Union
-from unittest.mock import patch, Mock
-from parameterized import parameterized
+from typing import Dict
+from unittest.mock import MagicMock, Mock, PropertyMock, patch
+from parameterized import parameterized, parameterized_class
+from requests import HTTPError
 
-from utils import access_nested_map, get_json, memoize
+from client import GithubOrgClient
+from fixtures import TEST_PAYLOAD
 
 
-class TestAccessNestedMap(unittest.TestCase):
-    """Tests the `access_nested_map` function."""
+class TestGithubOrgClient(unittest.TestCase):
+    """Tests the `GithubOrgClient` class."""
     @parameterized.expand([
-        ({"a": 1}, ("a",), 1),
-        ({"a": {"b": 2}}, ("a",), {"b": 2}),
-        ({"a": {"b": 2}}, ("a", "b"), 2),
+        ("google", {'login': "google"}),
+        ("abc", {'login': "abc"}),
     ])
-    def test_access_nested_map(self, nested_map: Dict, path: Tuple, expected: Union[Dict, int]):
-        """Tests `access_nested_map`'s output."""
-        self.assertEqual(access_nested_map(nested_map, path), expected)
-
-    @parameterized.expand([
-        ({}, ("a",), KeyError),
-        ({"a": 1}, ("a", "b"), KeyError),
-    ])
-    def test_access_nested_map_exception(self, nested_map: Dict, path: Tuple, exception: Exception):
-        """Tests `access_nested_map`'s exception raising."""
-        with self.assertRaises(exception):
-            access_nested_map(nested_map, path)
+    @patch("client.get_json")
+    def test_org(self, org, resp, mocked_fxn):
+        """Tests the `org` method."""
+        mocked_fxn.return_value = resp
+        gh_org_client = GithubOrgClient(org)
+        self.assertEqual(gh_org_client.org(), resp)
+        mocked_fxn.assert_called_once_with("https://api.github.com/orgs/{}".format(org))
 
 
-class TestGetJson(unittest.TestCase):
-    """Tests the `get_json` function."""
-    @parameterized.expand([
-        ("http://example.com", {"payload": True}),
-        ("http://holberton.io", {"payload": False}),
-    ])
-    def test_get_json(self, test_url: str, test_payload: Dict):
-        """Tests `get_json`'s output."""
-        attrs = {'json.return_value': test_payload}
-        with patch("requests.get", return_value=Mock(**attrs)) as req_get:
-            self.assertEqual(get_json(test_url), test_payload)
-            req_get.assert_called_once_with(test_url)
-
-
-class TestMemoize(unittest.TestCase):
-    """Tests the `memoize` function."""
-    class TestClass:
-        @memoize
-        def a_property(self):
-            return self.a_method()
-
-        def a_method(self):
-            return 42
-
-    @patch.object(TestClass, "a_method", return_value=42)
-    def test_memoize(self, memo_fxn):
-        """Tests `memoize`'s output."""
-        test_class = TestMemoize.TestClass()
-        self.assertEqual(test_class.a_property(), 42)
-        self.assertEqual(test_class.a_property(), 42)
-        memo_fxn.assert_called_once()
+if __name__ == '__main__':
+    unittest.main()
